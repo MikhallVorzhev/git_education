40c76af09e7703ff664ac9bc76f99395cd1fe8b9
mizhi <nulshh@gmail.com>
2024-09-19 10:49:15 +0300

    init files

---
+++ script.js
@@ -0,0 +1,12 @@
+function sayHi (){
+    alert(`Hello from Git!`);
+}
+
+function work (){
+    alert(`Work, work`)
+    //... work in progress ...
+}
+
+function sayBye(){
+    alert(`Bye from Git!`)
+}
